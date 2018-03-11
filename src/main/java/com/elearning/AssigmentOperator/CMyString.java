d1477693862243cc6dba3bc1571f3384d94ae22c
KuanYangGH <ykxjtuedu@163.com>
2018-03-11 21:40:46 +0800

    赋值运算符函数

---
+++ src/main/java/com/elearning/AssigmentOperator/CMyString.java
@@ -0,0 +1,38 @@
+package com.elearning.AssigmentOperator;
+
+public class CMyString {
+    private char[] m_pData;
+
+    public CMyString(char[] pData){
+        this.m_pData = pData;
+    }
+    public CMyString(CMyString cMyString){
+        this.m_pData = cMyString.getM_pData();
+    }
+    public CMyString(){
+
+    }
+
+    public char[] getM_pData() {
+        return m_pData;
+    }
+    public void printInfo(){
+        System.out.println(m_pData);
+    }
+
+    public static void main(String[] args){
+        char[] chars = {'h','e','l','l','o'};
+        CMyString a = new CMyString(chars);
+
+        CMyString b = new CMyString(a);
+        b.printInfo();
+
+        char[] chars2 = {'h','a','l','l','o'};
+        CMyString d = new CMyString(chars2);
+
+        CMyString c = d = a;
+        c.printInfo();
+        d.printInfo();
+    }
+}
+
