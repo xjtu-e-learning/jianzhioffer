5838e97de69652b3869f587146ddd32cf540d2fd
KuanYangGH <ykxjtuedu@163.com>
2018-05-08 16:07:45 +0800

    面试题16：反转链表

---
+++ src/main/java/com/elearning/chapter3/interviewquestion16/ListNode.java
@@ -0,0 +1,14 @@
+package src.main.java.com.elearning.chapter3.interviewquestion16;
+/**
+ * 链表节点
+ * @author yangkuan
+ */
+public class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}
