e5af6529ceeaa0d32d8f413850d8c0974727e9ea
KuanYangGH <ykxjtuedu@163.com>
2018-06-12 21:44:59 +0800

    面试题27：二叉搜索树和双向链表

---
+++ src/main/java/com/elearning/chapter4/interviewquestion27/TreeNode.java
@@ -0,0 +1,16 @@
+package com.elearning.chapter4.interviewquestion27;
+
+/**
+ * 树节点
+ * @author yangkuan
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
