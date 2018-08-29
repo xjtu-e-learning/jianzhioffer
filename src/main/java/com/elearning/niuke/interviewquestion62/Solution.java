a67fb9d6ea1f7fa15b7497303603c1ce90da9ec6
KuanYangGH <ykxjtuedu@163.com>
2018-08-29 16:22:27 +0800

    面试题62：序列化二叉树

---
+++ src/main/java/com/elearning/niuke/interviewquestion62/Solution.java
@@ -0,0 +1,68 @@
+package com.elearning.niuke.interviewquestion62;
+
+
+/**
+ * 面试题62：序列化二叉树
+ * 请实现两个函数，分别用来序列化和反序列化二叉树
+ *
+ * @author yangkuan
+ */
+public class Solution {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    int index = 0;
+
+    String Serialize(TreeNode root) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (root == null) {
+            stringBuilder.append("#,");
+            return stringBuilder.toString();
+        }
+        stringBuilder.append(root.val+",");
+        stringBuilder.append(Serialize(root.left));
+        stringBuilder.append(Serialize(root.right));
+        return stringBuilder.toString();
+    }
+
+    TreeNode Deserialize(String str) {
+        String[] strings = str.split(",");
+        return Deserialize(strings);
+    }
+    TreeNode Deserialize(String[] strings){
+        if(strings[index].equals("#")){
+            index++;
+            return null;
+        }
+        Integer val = Integer.valueOf(strings[index]);
+        TreeNode treeNode  = new TreeNode(val);
+        index++;
+        treeNode.left = Deserialize(strings);
+        treeNode.right = Deserialize(strings);
+        return treeNode;
+    }
+    public void test(){
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        String s = Serialize(a);
+        System.out.println(s);
+        TreeNode root = Deserialize(s);
+    }
+
+    public static void main(String[] args) {
+        new Solution().test();
+    }
+}
