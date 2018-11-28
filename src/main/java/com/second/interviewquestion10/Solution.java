7dd6efcf4e351ea0a29ccdc91f522645d1dd1922
KuanYangGH <ykxjtuedu@163.com>
2018-11-28 17:12:03 +0800

    二刷面试题10：二进制中1的个数

---
+++ src/main/java/com/second/interviewquestion10/Solution.java
@@ -0,0 +1,18 @@
+package com.second.interviewquestion10;
+
+/**
+ * 二进制中1的个数
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ *
+ * @author yangkuan
+ */
+public class Solution {
+    public int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+}
